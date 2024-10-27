f5b5ca444f307915271d37d1eab6bdab279e8d90
Reham_Mohamed <rehamatta1000@gmail.com>
2024-10-27 21:48:22 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var typed = new Typed("#typed", {
+    strings: ["developer", "designer", "freelancer", "Photographer"],
+    typeSpeed: 100, // Speed of typing (in milliseconds)
+    backSpeed: 50, // Speed of backspacing
+    loop: true, // Loop the typing animation
+    backDelay: 1000, // Delay before starting to backspace
+    startDelay: 500, // Delay before typing starts
+    showCursor: true, // Show blinking cursor
+    cursorChar: "|", // Customize the cursor character
+  });
